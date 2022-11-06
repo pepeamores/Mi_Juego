6475a50d78cc9e6df66f80b76b98e6a6c3e07bf5
pepeamores <pepe15sevilla@gmail.com>
2022-11-06 17:08:06 +0100

    segundo-juego

--- script.js
+++ script.js
@@ -17,7 +17,27 @@ opciones.push(["Sevilla FC", "Barcelona", "Madrid"]);
 let posActual = 0;
 //variable que guarda la cantidad acertadas hasta el moemento
 let cantidadAcertadas = 0;
+/*segundo Juego*/
+    // Array de palabras
+    var palabras = [["Sevilla", "Una Ciudad"], ["ordenador", "Una máquina"], ["valorant", "Un juego de ordenador"], ["negro", "Un color"], ["felicidad", "Un estado"]];
+    // Palabra a averiguar
+    var palabra = "";
+    // Nº aleatorio
+    var rand;
+    // Palabra oculta
+    var oculta = [];
+    // Elemento html de la palabra
+    var hueco = document.getElementById("palabra");
+    // Contador de intentos
+    var cont = 6;
+    // Botones de letras
+        var buttons = document.getElementsByClassName('letra');
+    // Boton de reset
+    var btnInicio = document.getElementById("reset");
+    
 
+
+document.getElementById("pantalla-juego2").style.display= "none"
 function comenzarJuego(){
     //reseteamos las variables
     posActual = 0;
@@ -25,6 +45,7 @@ function comenzarJuego(){
     //activamos las pantallas necesarias
     document.getElementById("pantalla-inicial").style.display = "none";
     document.getElementById("pantalla-juego").style.display = "block";
+    document.getElementById("pantalla-juego2").style.display= "none";
     cargarPregunta();
 
 }
@@ -83,9 +104,105 @@ function terminarJuego(){
     document.getElementById("numIncorrectas").innerHTML = preguntas.length - cantidadAcertadas;
 }
 
-function volverAlInicio(){
+function irSegundoJuego(){
     //ocultamos las pantallas y activamos la inicial
     document.getElementById("pantalla-final").style.display = "none";
-    document.getElementById("pantalla-inicial").style.display = "block";
+    document.getElementById("pantalla-inicial").style.display = "none";
     document.getElementById("pantalla-juego").style.display = "none";
+    document.getElementById("pantalla-juego2").style.display = "block";
+
+    generaPalabra();
+    pintarGuiones(palabra.length);
+    generaABC("a","z");
 }
+cont = 6;
+    document.getElementById("intentos").innerHTML=cont;
+    function generaPalabra() {
+        rand = (Math.random() * 19).toFixed(0);
+        palabra = palabras[rand][0].toUpperCase();
+        console.log(palabra);
+      }
+      
+      // Funcion para pintar los guiones de la palabra
+      function pintarGuiones(num) {
+        for (var i = 0; i < num; i++) {
+          oculta[i] = "_";
+        }
+        hueco.innerHTML = oculta.join("");
+      }
+      
+      //Generar abecedario
+      function generaABC (a,z) {
+        document.getElementById("abcdario").innerHTML = "";
+        var i = a.charCodeAt(0), j = z.charCodeAt(0);
+        var letra = "";
+        for( ; i<=j; i++) {
+          letra = String.fromCharCode(i).toUpperCase();
+          document.getElementById("abcdario").innerHTML += "<button value='" + letra + "' onclick='intento(\"" + letra + "\")' class='letra' id='"+letra+"'>" + letra + "</button>";
+          if(i==110) {
+            document.getElementById("abcdario").innerHTML += "<button value='Ñ' onclick='intento(\"Ñ\")' class='letra' id='"+letra+"'>Ñ</button>";
+          }
+        }
+      }
+      
+      // Chequear intento
+      function intento(letra) {
+        document.getElementById(letra).disabled = true;
+        if(palabra.indexOf(letra) != -1) {
+          for(var i=0; i<palabra.length; i++) {
+            if(palabra[i]==letra) oculta[i] = letra;
+          }
+          hueco.innerHTML = oculta.join("");
+          document.getElementById("acierto").innerHTML = "Bien!";
+          document.getElementById("acierto").className += "acierto verde";
+        }else{
+          cont--;
+          document.getElementById("intentos").innerHTML = cont;
+          document.getElementById("acierto").innerHTML = "Fallo!";
+          document.getElementById("acierto").className += "acierto rojo";
+          document.getElementById("image"+cont).className += "fade-in";
+        }
+        compruebaFin();
+        setTimeout(function () { 
+          document.getElementById("acierto").className = ""; 
+        }, 800);
+      }
+      
+      // Obtener pista
+      function pista() {
+        document.getElementById("hueco-pista").innerHTML = palabras[rand][1];
+      }
+      
+      // Compruba si ha finalizado
+      function compruebaFin() {
+        if( oculta.indexOf("_") == -1 ) {
+          document.getElementById("msg-final").innerHTML = "Felicidades !!";
+          document.getElementById("msg-final").className += "zoom-in";
+          document.getElementById("palabra").className += " encuadre";
+          for (var i = 0; i < buttons.length; i++) {
+            buttons[i].disabled = true;
+          }
+          document.getElementById("reset").innerHTML = "Empezar";
+          btnInicio.onclick = function() { location.reload() };
+        }else if( cont == 0 ) {
+          document.getElementById("msg-final").innerHTML = "Game Over";
+          document.getElementById("msg-final").className += "zoom-in";
+          for (var i = 0; i < buttons.length; i++) {
+            buttons[i].disabled = true;
+          }
+          document.getElementById("reset").innerHTML = "Empezar";
+          btnInicio.onclick = function () { location.reload() };
+        }
+      }
+      
+      // Restablecer juego
+      function inicio() {
+        generaPalabra();
+        pintarGuiones(palabra.length);
+        generaABC("a","z");
+        cont = 6;
+        document.getElementById("intentos").innerHTML=cont;
+      }
+      
+      // Iniciar
+      window.onload = inicio();
