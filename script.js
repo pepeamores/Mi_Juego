1c03b0b196ffb192769482a52c773558d5fa3c8a
pepeamores <pepe15sevilla@gmail.com>
2022-11-07 13:29:06 +0100

    tercer-juego

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let posActual = 0;
 let cantidadAcertadas = 0;
 /*segundo Juego*/
     // Array de palabras
-    var palabras = [["Sevilla", "Una Ciudad"], ["ordenador", "Una máquina"], ["valorant", "Un juego de ordenador"], ["negro", "Un color"], ["felicidad", "Un estado"]];
+    var palabras = [["Sevilla", "Una Ciudad"], ["ordenador", "Algo que utilizamos siempre"], ["valorant", "Un juego de ordenador"], ["negro", "Un color"], ["felicidad", "Un estado"]];
     // Palabra a averiguar
     var palabra = "";
     // Nº aleatorio
@@ -34,10 +34,25 @@ let cantidadAcertadas = 0;
         var buttons = document.getElementsByClassName('letra');
     // Boton de reset
     var btnInicio = document.getElementById("reset");
-    
+/* Tercer Juego*/
+var cartas = new Array( 
+  {nombre: 'Sevilla', seleccion: false}, {nombre: 'Rodrigo', seleccion: false}, 
+  {nombre: 'Futbol', seleccion: false}, {nombre: 'Eugenia', seleccion: false}, 
+  {nombre: 'Maria Victoria', seleccion: false}, {nombre: 'Antonio', seleccion: false}, 
+  {nombre: 'Carlos Eduardo', seleccion: false}, {nombre: 'Carmen Elena', seleccion: false}, 
+  {nombre: 'Sevilla', seleccion: false}, {nombre: 'Rodrigo', seleccion: false}, 
+  {nombre: 'Futbol', seleccion: false}, {nombre: 'Eugenia', seleccion: false}, 
+  {nombre: 'Maria Victoria', seleccion: false}, {nombre: 'Antonio', seleccion: false}, 
+  {nombre: 'Carlos Eduardo', seleccion: false}, {nombre: 'Carmen Elena', seleccion: false} );
+        
+var intentos = 0;
+var jugada1 = "";
+var jugada2 = "";
+var identificadorJ1 = "";
+var identificadorJ2 = "";
 
-
-document.getElementById("pantalla-juego2").style.display= "none"
+document.getElementById("pantalla-juego2").style.display= "none";
+document.getElementById("pantalla-juego3").style.display= "none";
 function comenzarJuego(){
     //reseteamos las variables
     posActual = 0;
@@ -46,10 +61,11 @@ function comenzarJuego(){
     document.getElementById("pantalla-inicial").style.display = "none";
     document.getElementById("pantalla-juego").style.display = "block";
     document.getElementById("pantalla-juego2").style.display= "none";
+    document.getElementById("pantalla-juego3").style.display= "none";
     cargarPregunta();
 
 }
-
+document.getElementById("pantalla-juego2").style.display= "none";
 //funcion que carga la siguiente bandera y sus opciones
 function cargarPregunta(){
     //controlo sis se acabaron las banderas
@@ -110,6 +126,7 @@ function irSegundoJuego(){
     document.getElementById("pantalla-inicial").style.display = "none";
     document.getElementById("pantalla-juego").style.display = "none";
     document.getElementById("pantalla-juego2").style.display = "block";
+    document.getElementById("pantalla-juego3").style.display= "none";
 
     generaPalabra();
     pintarGuiones(palabra.length);
@@ -206,3 +223,117 @@ cont = 6;
       
       // Iniciar
       window.onload = inicio();
+function irTercerJuego(){
+        //ocultamos las pantallas y activamos la inicial
+        document.getElementById("pantalla-final").style.display = "none";
+        document.getElementById("pantalla-inicial").style.display = "none";
+        document.getElementById("pantalla-juego").style.display = "none";
+        document.getElementById("pantalla-juego2").style.display = "none";
+        document.getElementById("pantalla-juego3").style.display = "block";  
+        iniciarJuego ();
+        resetearJuego ();
+        girarCarta ();
+        vaciar ();
+        colorCambio (posicion, color, contenido);
+        comprobar ();
+        resetearJuego ();
+        
+}
+console.profile()
+
+function iniciarJuego () {  
+  var dato = document.getElementById("juego");
+  dato.style.opacity = 1;
+
+  cartas.sort(function() {return Math.random() - 0.5});
+  for ( var i = 0 ; i < 16 ; i++ ) {
+    var carta = cartas[i].nombre;
+    var dato = document.getElementById( i.toString() );
+    dato.dataset.valor = carta;
+  }
+};
+
+function resetearJuego () {
+  cartas.sort(function() {return Math.random() - 0.5});
+  for ( var i = 0 ; i < 16 ; i++ ) {
+    var carta = cartas[i].nombre;
+    var dato = document.getElementById( i.toString() );
+    dato.dataset.valor = carta;
+    colorCambio( i, 'black', '?');
+  } 
+}
+
+function girarCarta () {
+  var evento = window.event;
+
+  jugada2 = evento.target.dataset.valor;
+  identificadorJ2 = evento.target.id;
+
+
+  if ( jugada1 !== "" ) {
+
+    if ( jugada1 === jugada2 && identificadorJ1 !== identificadorJ2 && cartas[parseInt(identificadorJ2)].seleccion != true &&               cartas[parseInt(identificadorJ1)].seleccion != true) {
+      
+      cartas[parseInt(identificadorJ1)].seleccion = true;
+      cartas[parseInt(identificadorJ2)].seleccion = true;
+
+      colorCambio(identificadorJ2, "blue", jugada2);
+      vaciar();
+      comprobar();
+    }else if(identificadorJ1 !== identificadorJ2){
+      var self = this;
+      setTimeout(function(){
+        colorCambio(self.identificadorJ1, "black", "?")
+        colorCambio(self.identificadorJ2, "black", "?")
+        vaciar()
+      },200); 
+
+      colorCambio(identificadorJ2, "blue", jugada2);
+    }
+  } else if(jugada2 !== "valor") {
+
+    colorCambio(identificadorJ2, "blue", jugada2);
+
+    jugada1 = jugada2;
+    identificadorJ1 = identificadorJ2;
+  }
+};
+
+function vaciar ()  {
+  jugada1 = ""; 
+  jugada2 = ""; 
+
+  identificadorJ1 = "";
+  identificadorJ2 = "";
+}
+
+function colorCambio (posicion, color, contenido) {
+  document.getElementById(posicion.toString()).style.backgroundColor = color;
+  document.getElementById(posicion.toString()).innerHTML = contenido;
+}   
+
+function comprobar () {
+  var aciertos = 0;
+  for( var i = 0 ; i < 16 ; i++ ){
+    if ( cartas[i].seleccion == true ) {
+      aciertos ++;
+    }
+
+  }
+
+  if(aciertos == 16){
+   document.getElementById("juego").innerHTML = "Felicidades Ganaste!!!";
+     
+  }
+}
+
+function resetearJuego () {
+            cartas.sort(function() { return Math.random() - 0.5});
+            for ( var i = 0; i < 16 ; i++ ) {
+                var carta = cartas[i].nombre;
+                var dato = document.getElementById( i.toString() );
+                dato.dataset.valor = carta;
+                colorCambio(i, 'black', '?');
+              console.profileEnd()
+            }
+};
